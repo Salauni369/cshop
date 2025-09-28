04903098d1e0f3c342a698e83f3fa6734cf00fc0
Salauni369 <salonichaudhary159@gmail.com>
2025-09-29 00:08:56 +0530

    Update setting_screen.dart

--- lib/pages/setting_screen.dart
+++ lib/pages/setting_screen.dart
@@ -1,66 +1,3 @@
-// import 'package:flutter/material.dart';
-//
-// class SettingsPage extends StatefulWidget {
-//   const SettingsPage({super.key});
-//
-//   @override
-//   State<SettingsPage> createState() => _SettingsPageState();
-// }
-//
-// class _SettingsPageState extends State<SettingsPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Center(child: const Text("Settings",style: TextStyle(color: Colors.white),)),
-//         backgroundColor: Colors.brown,
-//       ),
-//       body: ListView(
-//         children: [
-//           const ListTile(
-//             leading: Icon(Icons.person),
-//             title: Text("Account"),
-//             subtitle: Text("Privacy, Security, Hide Orders"),
-//
-//
-//           ),
-//           const Divider(),
-//
-//           const ListTile(
-//             leading: Icon(Icons.notifications),
-//             title: Text("Notifications"),
-//             subtitle: Text("Message, group & call tones"),
-//           ),
-//           const Divider(),
-//
-//           SwitchListTile(
-//             secondary: const Icon(Icons.dark_mode),
-//             title: const Text("Dark Mode"),
-//             value: true, // set default
-//             onChanged: (bool value) {
-//
-//             },
-//           ),
-//           const Divider(),
-//
-//           ListTile(
-//             leading: const Icon(Icons.info),
-//             title: const Text("About"),
-//             onTap: () {
-//               showAboutDialog(
-//                 context: context,
-//                 applicationName: "Coffee App",
-//                 applicationVersion: "1.0.0",
-//                 applicationLegalese: "© 2025 Coffee Lovers Inc.",
-//               );
-//             },
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 
 import 'package:flutter/material.dart';
 
@@ -99,9 +36,7 @@ class _SettingsPageState extends State<SettingsPage> {
             title: Text("Notifications"),
             subtitle: Text("Message, group & call tones"),
           ),
-          const Divider(),
-
-          // Dark Mode Switch
+          const Divider(), 
           SwitchListTile(
             secondary: const Icon(Icons.dark_mode),
             title: const Text("Dark Mode"),
@@ -113,8 +48,6 @@ class _SettingsPageState extends State<SettingsPage> {
             },
           ),
           const Divider(),
-
-          // About Section
           ListTile(
             leading: const Icon(Icons.info),
             title: const Text("About"),
